afd80581e022b9f6e7357b6fdacb1ca6cfd16cc5
Steve Aguet <psyaosan@gmail.com>
2014-07-14 13:32:20 +0200

    Added essentials dependencies

--- app/config/app.php
+++ app/config/app.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'url' => 'http://localhost',
+	'url' => 'http://psyao.dev',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -39,7 +39,7 @@ return array(
 	|
 	*/
 
-	'timezone' => 'UTC',
+	'timezone' => 'Europe/Zurich',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => '03fvmg2E3LbML9xouFBMl4FqtvI41uTD',
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
