28518b5e951445d098c269f39b5ed25598c12170
Steve Aguet <psyaosan@gmail.com>
2014-07-16 01:41:36 +0200

    Added footer

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>psyao.ch : Curriculum  vitae en ligne de Steve Aguet</title>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic" <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/main.css"/>
 </head>
 <body>
@@ -13,6 +14,8 @@
 <div class="container">
     @yield('content')
 </div>
+
+@include('layouts.partials.footer')
 
 <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
---
+++ app/views/layouts/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div class="container">
+        <p>Created by <a href="http://www.elysion.ch">Elysion Workshop</a></p>
+    </div>
+</footer>
