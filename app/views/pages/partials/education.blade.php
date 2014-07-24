7c65b7db6c14127cb70bd149ea4d5159252517f2
Steve Aguet <psyaosan@gmail.com>
2014-07-25 01:33:39 +0200

    Moved occupation and education section to partial

---
+++ app/views/pages/partials/education.blade.php
@@ -0,0 +1,54 @@
+<!-- Eduction -->
+<div class="row resume-element">
+    <div id="education" class="anchor">&nbsp;</div>
+    <div class="col-sm-3 col-lg-2 col-lg-offset-1">
+        <h3><span class="fa fa-university"></span> Formation</h3>
+    </div>
+    <div class="col-sm-9">
+        @unless($institutes->count())
+        <div class="row">
+            <div class="col-sm-12">
+                <p class="item">Il n'y a aucun établissement!</p>
+            </div>
+        </div>
+        @else
+        @foreach($institutes as $institute)
+        <div class="row resume-element-item">
+            <div class="col-sm-12">
+                <h4>{{ $institute->name }}, {{ $institute->location }}</h4>
+            </div>
+
+            @unless($institute->courses->count())
+            <div class="col-sm-12 item">
+                <p>Il n'y a aucune orientation!</p>
+            </div>
+            @else
+            @foreach($institute->courses as $course)
+            <div class="col-sm-8 item">
+                <h5>{{ $course->title }}</h5>
+
+                <ul>
+                    <li>{{ $course->domain }}</li>
+                    @if( $course->diploma )
+                    <li><em>{{ $course->diploma }}</em></li>
+                    @endif
+                </ul>
+            </div>
+
+            <div class="col-sm-4">
+                <p>{{ $course->from->format('m.Y') }} -
+                    @if( ! is_null($course->to))
+                    {{ $course->to->format('m.Y') }}
+                    @else
+                    A ce jour
+                    @endif
+                </p>
+            </div>
+            @endforeach
+            @endunless
+        </div>
+        @endforeach
+        @endunless
+    </div>
+</div>
+<!-- /Eduction -->
---
+++ app/views/pages/partials/occupation.blade.php
@@ -0,0 +1,50 @@
+<!-- Occupation -->
+<div class="row resume-element">
+    <div class="col-sm-3 col-lg-2 col-lg-offset-1">
+        <h3><span class="fa fa-building"></span> Expériences</h3>
+    </div>
+    <div class="col-sm-9">
+        @unless($companies->count())
+        <div class="row">
+            <div class="col-sm-12">
+                <p class="item">Il n'y a aucune expérience!</p>
+            </div>
+        </div>
+        @else
+        @foreach($companies as $company)
+        <div class="row resume-element-item">
+            <div class="col-sm-12">
+                <h4>{{ $company->name }}, {{ $company->location }}</h4>
+            </div>
+
+            @unless($company->jobs->count())
+            <div class="col-sm-12 item">
+                <p>Il n'y a aucun emploi!</p>
+            </div>
+            @else
+            @foreach($company->jobs as $job)
+            <div class="col-sm-8 item">
+                <h5>{{ $job->title }}</h5>
+
+                @if( $job->description )
+                <p>{{ $job->description }}</p>
+                @endif
+            </div>
+
+            <div class="col-sm-4">
+                <p>{{ $job->from->format('m.Y') }} -
+                    @if( ! is_null($job->to))
+                    {{ $job->to->format('m.Y') }}
+                    @else
+                    A ce jour
+                    @endif
+                </p>
+            </div>
+            @endforeach
+            @endunless
+        </div>
+        @endforeach
+        @endunless
+    </div>
+</div>
+<!-- /Occupation -->
